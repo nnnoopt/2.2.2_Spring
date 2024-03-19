94dac8465e1fab47403772ee9a32b7a52a0343d8
nnnoopt <nnnoopt@yandex.ru>
2024-03-19 19:46:11 +0300

    2.2.2 update

--- src/main/java/web/services/CarServiceImpl.java
+++ src/main/java/web/services/CarServiceImpl.java
@@ -1,18 +1,30 @@
 package web.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import web.dao.CarDao;
 import web.models.Car;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CarServiceImpl implements CarService {
-    @Autowired
-    private CarDao carDao;
+
+    private List<Car> cars = new ArrayList<>();
+
+    {
+        cars.add(new Car("BMW", 2020, "Black"));
+        cars.add(new Car("Mercedes", 2021, "White"));
+        cars.add(new Car("Tesla", 2022, "Black"));
+        cars.add(new Car("Toyota", 2023, "Black"));
+        cars.add(new Car("Lexus", 2024, "Black"));
+    }
+
     @Override
     public List<Car> getCars(int count) {
-        return carDao.getCarsCount(count);
+        if (count >= cars.size() || count == 0) {
+            return cars;
+        }
+        return cars.stream().limit(count).collect(Collectors.toList());
     }
 }
